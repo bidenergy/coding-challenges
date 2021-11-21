c11047048fbbb5c18fafdb2f121ae0eb141ae906
Philip Gautam <philip.gautam@gmail.com>
2021-11-21 19:19:12 +1100

    Implement test for scanning all example products.

--- Supermarket/Supermarket.Test/CheckoutTest.cs
+++ Supermarket/Supermarket.Test/CheckoutTest.cs
@@ -14,7 +14,15 @@ namespace Supermarket.Test
         {
             var mockRepo = new Mock<IDataRepository>();
 
-            mockRepo.Setup(r => r.LookupProduct("A")).Returns(new Product("A", 50));
+            var productA = new Product("A", 50);
+            var productB = new Product("B", 30);
+            var productC = new Product("C", 20);
+            var productD = new Product("D", 15);
+
+            mockRepo.Setup(r => r.LookupProduct("A")).Returns(productA);
+            mockRepo.Setup(r => r.LookupProduct("B")).Returns(productB);
+            mockRepo.Setup(r => r.LookupProduct("C")).Returns(productC);
+            mockRepo.Setup(r => r.LookupProduct("D")).Returns(productD);
 
             _checkout = new Checkout(mockRepo.Object);
         }
@@ -48,16 +56,16 @@ namespace Supermarket.Test
         }
 
         [Test]
-        public void ScanSameProductTwice()
+        public void ScanAllExampleProducts()
         {
             _checkout.Scan("A");
-            _checkout.Scan("A");
+            _checkout.Scan("B");
+            _checkout.Scan("C");
+            _checkout.Scan("D");
 
             var price = _checkout.GetTotalPrice();
 
-            Assert.That(price, Is.EqualTo(100));
+            Assert.That(price, Is.EqualTo(115));
         }
-
-
     }
 }

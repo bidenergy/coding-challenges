d500e3c0dd552248aa38a6d1ac291dcc8baf0f17
manjunathrbhat <manjunathrbhat024@gmail.com>
2022-01-25 21:47:56 +1100

    Logic to find the robot position and getHeading method is added

--- RobotWars/Program.cs
+++ RobotWars/Program.cs
@@ -19,5 +19,48 @@ public class Program
 		List<String> output = new List<String>();
 		
 		
+		// Logic to find the position of robots
+		for(int i = 1; i < inputArray.Length; i=i+2) {
+			
+			// Robot's x,y coordinate and heading is stored in variables
+			string position = inputArray[i];
+			int currentPositionX = Convert.ToInt32(position.Split(' ')[0]);
+			int currentPositionY = Convert.ToInt32(position.Split(' ')[1]);
+			string currentHeading = position.Split(' ')[2];
+			
+			int nextPositionX, nextPositionY;
+			string nextHeading;
+			
+			// nextPosition of robot is calculated based on current heading of the robot
+			if(currentHeading.Equals("N")) {
+				nextPositionX = currentPositionX;
+				nextPositionY = currentPositionY + 1;
+			} else if (currentHeading.Equals("E")) {
+				nextPositionX = currentPositionX + 1;
+				nextPositionY = currentPositionY;
+			} else if (currentHeading.Equals("S")) {
+				nextPositionX = currentPositionX;
+				nextPositionY = currentPositionY - 1;
+			} else if (currentHeading.Equals("W")) {
+				nextPositionX = currentPositionX - 1;
+				nextPositionY = currentPositionY;
+			}
+			
+			// Robot's position is stored in a variable
+			string instructions = inputArray[i+1];
+		}
+		
+		// Method that returns heading of the robot based on current position and next position
+		string getHeading(int currentPositionX, int currentPositionY, int nextPositionX, int nextPositionY) {
+			string heading = "";
+			
+			if(currentPositionY == nextPositionY+1) heading = "N";
+			else if(currentPositionX == nextPositionX+1) heading = "E";
+			else if(currentPositionY == nextPositionY-1) heading = "S";
+			else if(currentPositionX == nextPositionX-1) heading = "W";
+			
+			return heading;
+		}	
+		
 	}
 }
